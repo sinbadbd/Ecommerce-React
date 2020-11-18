747f0267dec35873a2ecc07370791bc83e78c0d5
imran <imran.mia@portonics.com>
2020-11-18 23:28:21 +0600

    filter

--- src/Component/Filter.js
+++ src/Component/Filter.js
@@ -1,28 +1,32 @@
 import React, {Component} from "react"
-
+import {Row, coloumn} from 'react-bootstrap'
 class Filter extends Component {
     render(){
         return (
-             <div className="row py-4">
-                 <div className="col-2 filter-result">
+
+            <Row>
+                <Col md={2}>
+                <div className="col-2 filter-result">
                     Total: {this.props.count}
                 </div>
-                 <div className="col-5 filter-sort">
-                    <select className="custom-select" value={this.props.sort} onChange={this.props.sortProducts}>
+                </Col>
+                <Col md={5}>
+                <select className="custom-select" value={this.props.sort} onChange={this.props.sortProducts}>
                         <option className >Open this select menu</option>
                         <option value="1">Lowest Price</option>
                         <option value="2">Highest Price</option>
                     </select>
-                 </div>
-                 <div className="col-5 filter-size">
-                    <select className="custom-select"  value={this.props.size} onChange={this.props.filterProducts}>
-                            <option className>Open this select menu</option>
+                </Col>
+                <Col md={5}>
+                <select className="custom-select"  value={this.props.size} onChange={this.props.filterProducts}>
+                            <option className>Filter size</option>
                             <option value="L">L</option>
                             <option value="M">M</option>
                             <option value="XL">XL</option>
                         </select>
-                 </div>
-             </div>
+                </Col>
+            </Row>
+              
         )
     }
 }
